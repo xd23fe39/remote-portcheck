27bd1d03f41b50122fbb8fdbde2f647d7bd88706
Frank <xd23fe39@yahoo.de>
2018-05-25 14:55:53 +0200

    Create portcheck.sh

---
+++ bin/portcheck.sh
@@ -0,0 +1,33 @@
+# REMOTE PortChecker
+# - wird von einem zentralen Rechner installiert und aufgerufen
+# - prüft von dort aus alle SourceIP-TargetIP-Port-Kombinationen.
+# - erfordert ssh und openssl auf allen Systemen
+#
+echo "REMOTE PortChecker, FF 2018"
+echo
+# SourceIP Schleife
+for s in $1; do
+    echo "Source IP: $s"
+    echo
+    echo "Target IP(s):"
+    # QuellIP-Schleife
+    for i in $2; do
+        # Ports-Schleife
+        for p in $3; do
+            # Aufruf unterstützt z.B. Disconnect-OpenSSL-by-Q, Timeout bei FW-Blocking
+            ssh -x $s "((echo Q | timeout 2 openssl s_client -connect $i:$p) > /dev/null 2>&1 )"
+            RES=$?
+            if [ "$RES" == "0" ]; then
+                echo "  $s $i $p: OK"
+            elif [ "$RES" == "124" ]; then
+                echo "  $s $i $p: OK=$RES"
+            else
+                # FW blockiert oder: unbekannter Fehlercode in $RES
+                echo "  $s $i $p: FAIL=$RES"
+            fi
+        done
+    done
+    echo
+done
+echo "Test completed!"
+echo
